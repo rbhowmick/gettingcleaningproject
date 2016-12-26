1fc044f2da97c698803bcc05b693c851f426cb69
rbhowmick <rajb007@me.com>
2016-12-25 23:12:34 -0500

    fourth commit

---
+++ run_analysis.R
@@ -0,0 +1,16 @@
+library(dplyr)
+activity_labels<-(read.table("activity_labels.txt"))[["V2"]]
+column_names<-gsub("\\(\\)","",(read.table("features.txt"))[["V2"]])
+test_data_x<-read.table("test/X_test.txt", col.names=column_names)
+test_data_y<-read.table("test/y_test.txt",col.names={"activity_label"})
+subject_test<-read.table("test/subject_test.txt",col.names={"subject"})
+test_data_x<-cbind(test_data_x,subject_test,test_data_y)
+train_data_x<-read.table("train/X_train.txt",col.names=column_names)
+train_data_y<-read.table("train/y_train.txt",col.names={"activity_label"})
+subject_train<-read.table("train/subject_train.txt",col.names={"subject"})
+train_data_x<-cbind(train_data_x,subject_train,train_data_y)
+merged_data<-merge(train_data_x,test_data_x,all=TRUE)
+mean_stddev_measurements<-merged_data[,grep("mean$|mean[-,\\.]+|std|subject|activity_label",names(merged_data))]
+mean_stddev_measurements<-mutate(mean_stddev_measurements,activity_label=activity_labels[activity_label])
+summarized_measurements<-mean_stddev_measurements %>% group_by(subject,activity_label) %>% summarize_each(funs(mean))
+write.table(summarized_measurements,file="tidy_data_rajesh.txt",row.name=FALSE)
